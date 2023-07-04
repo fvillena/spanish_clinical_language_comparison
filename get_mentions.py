a67c94c1f1bf8eaa4e1715ef6a2571e436fa3178
Fabián Villena <villenafabian@gmail.com>
2023-07-04 13:05:57 +0000

    mention frequency

--- get_mentions.py
+++ get_mentions.py
@@ -11,5 +11,6 @@ if __name__ == "__main__":
     mentions = abstracts.mentions + eudract.mentions
     data = pd.DataFrame(mentions, columns = ["code","corpus","text"])
     data["text"] = data.text.str.lower().apply(unidecode)
-    data.sort_values(["code","corpus"], inplace=True)
+    data = data.groupby(["code","corpus","text"]).size().reset_index().rename(columns={0:"frequency"})
+    data.sort_values(["code","corpus","frequency"], inplace=True)
     data.to_excel("data/processed/mentions.xlsx", index=False)
