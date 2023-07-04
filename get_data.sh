932154f1748b861a85c8eb3407523bff69907d3c
Fabián Villena <fvillena@proton.me>
2023-07-04 21:54:46 +0000

    CWLsC

--- get_data.sh
+++ get_data.sh
@@ -3,5 +3,6 @@
 wget -nc -P /tmp http://www.lllf.uam.es/ESP/nlpdata/wp2/CT-EBM-SP.zip
 unzip -j /tmp/CT-EBM-SP.zip "train/abstracts/*" -d ./data/raw/abstracts
 unzip -j /tmp/CT-EBM-SP.zip "train/eudract/*" -d ./data/raw/eudract
+wget -nc -P /tmp https://zenodo.org/record/7757971/files/CWLsC.zip
+unzip -j /tmp/CWLsC.zip "CWLsC/ann/*" -d ./data/raw/cwlsc
 google-ngram-downloader download -l spa -o data/raw/google-unigrams
-#google-ngram-downloader readline -n 1 -l spa > data/raw/google-unigrams.tsv
