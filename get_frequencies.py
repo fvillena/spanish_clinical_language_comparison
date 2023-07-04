6c085e8b82c60b470cd2deb95f5f8d4346b46586
Fabián Villena <fvillena@proton.me>
2023-07-04 12:03:49 +0000

    google ngrams

--- get_frequencies.py
+++ get_frequencies.py
@@ -4,6 +4,7 @@ from pathlib import Path
 from collections import Counter
 import pandas as pd
 from unidecode import unidecode
+import json
 
 if __name__ == "__main__":
     abstracts = Corpus(Path("data/raw/abstracts"))
@@ -16,6 +17,7 @@ if __name__ == "__main__":
         except LookupError:
             import nltk
             nltk.download('punkt')
+            tokenized_mention = word_tokenize(mention[2])
         corpus = mention[1]
         for word in tokenized_mention:
             word = unidecode(word.lower())
@@ -23,7 +25,20 @@ if __name__ == "__main__":
                 vocabulary[word].update([corpus])
             else:
                 vocabulary[word] = Counter([corpus])
-    frequencies = pd.DataFrame.from_dict(vocabulary, orient="index")
+    with open('data/processed/unigrams.json') as f:
+        unigram_frequency = json.load(f)
+    unigram_frequency_processed = {}
+    for word,frequency in unigram_frequency.items():
+        if word in unigram_frequency_processed:
+            unigram_frequency_processed[word] + frequency
+        else:
+            unigram_frequency_processed[word] = frequency
+    for word,counter in vocabulary.items():
+        try:
+            counter.update(google = unigram_frequency_processed[word])
+        except:
+            continue
+    frequencies = pd.DataFrame.from_dict(vocabulary, orient="index").fillna(0)
     frequencies = frequencies.melt(var_name="corpus",value_name="frequency", ignore_index=False)
     frequencies.sort_values(["corpus"], inplace=True)
     frequencies.sort_index(inplace=True)
