9ac20ef151ddf96281763883c88c9cb976614d6e
Fabián Villena <fvillena@proton.me>
2023-07-07 15:21:09 +0000

    normalized frequency

--- get_frequencies.py
+++ get_frequencies.py
@@ -7,6 +7,8 @@ from collections import Counter
 import pandas as pd
 import json
 from src import preprocess
+from sklearn.preprocessing import QuantileTransformer
+import numpy as np
 
 if __name__ == "__main__":
     abstracts = Corpus(Path("data/raw/abstracts"))
@@ -45,7 +47,11 @@ if __name__ == "__main__":
         except:
             continue
     frequencies = pd.DataFrame.from_dict(vocabulary, orient="index").fillna(0)
-    frequencies = frequencies.melt(var_name="corpus",value_name="frequency", ignore_index=False)
-    frequencies.sort_values(["corpus"], inplace=True)
-    frequencies.sort_index(inplace=True)
-    frequencies.to_excel("data/processed/frequencies.xlsx", index_label="word")
+    scaler = QuantileTransformer(n_quantiles=10000)
+    frequencies_scaled = frequencies.applymap(lambda x: np.log2(x+1))
+    frequencies_scaled[:] = scaler.fit_transform(frequencies)
+    frequencies = frequencies.melt(var_name="corpus",value_name="frequency", ignore_index=False).reset_index(names="word")
+    frequencies_scaled = frequencies_scaled.melt(var_name="corpus",value_name="frequency_scaled", ignore_index=False).reset_index(names="word")
+    frequencies = frequencies.merge(frequencies_scaled, on=["word","corpus"])
+    frequencies.sort_values(["word","corpus"], inplace=True)
+    frequencies.to_excel("data/processed/frequencies.xlsx", index=False)
