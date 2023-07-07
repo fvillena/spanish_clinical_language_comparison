de9e8e30241726907ccd4aaecb13b75b0a0fd918
Fabián Villena <fvillena@proton.me>
2023-07-07 23:26:42 +0000

    multiprocess

--- compute_umls_unigrams.py
+++ compute_umls_unigrams.py
@@ -1,34 +1,56 @@
 #! /usr/local/python/current/bin/python
 
-import pandas as pd
 from nltk.tokenize import word_tokenize
 from src import preprocess
 import json
-from sklearn.feature_extraction.text import CountVectorizer
-import numpy as np
+from concurrent.futures import ProcessPoolExecutor, as_completed
+from multiprocessing import cpu_count
+from math import ceil
+from collections import Counter
+import csv
 
-class NpEncoder(json.JSONEncoder):
-    def default(self, obj):
-        if isinstance(obj, np.integer):
-            return int(obj)
-        if isinstance(obj, np.floating):
-            return float(obj)
-        if isinstance(obj, np.ndarray):
-            return obj.tolist()
-        return super(NpEncoder, self).default(obj)
+lines = []
+N = 0
+C = cpu_count()
 
-data = pd.read_csv('data/raw/MRCONSO_202307050503.csv')
+with open('data/raw/MRCONSO_202307050503.csv', mode='r', encoding='utf-8', newline='') as f:
+    reader = csv.reader(f, delimiter=',', quotechar='"')
+    next(reader, None)
+    for i,row in enumerate(reader):
+        # if i == 1000000:
+        #     break
+        N += 1
+        lines.append(row[1])
 
-cv = CountVectorizer(
-    preprocessor=preprocess,
-    tokenizer=word_tokenize
-)
+BATCH_SIZE = ceil(N/C)
 
-x = cv.fit_transform(data.STR)
+def process(batch, function):
+    processed_batch = []
+    for element in batch:
+        processed_batch.append(function(element))
+    return processed_batch
 
 
-vocab = list(cv.get_feature_names_out())
-count = dict(zip(vocab, x.sum(axis=0).A1))
+def execute_in_parallel(function, l, max_workers=C, batch_size=BATCH_SIZE):
+    with ProcessPoolExecutor(max_workers=max_workers) as executor:
+        result = []
+        futures = []
+        for i in range(0, len(l), batch_size):
+            batch = l[i:i + batch_size]
+            futures.append(executor.submit(process, batch, function))
+        for future in as_completed(futures):
+            result.extend(future.result())
+    return result
 
-with open("data/interim/umls_unigrams.json", mode="w", encoding="utf-8") as f:
-    f.write(json.dumps(count, ensure_ascii=False, cls=NpEncoder))
+preprocessed_lines = execute_in_parallel(preprocess, lines)
+tokenized_lines = execute_in_parallel(word_tokenize, preprocessed_lines)
+counters = execute_in_parallel(Counter, tokenized_lines)
+
+#sum counters in pair then merge
+counter = counters[0]
+for c in counters[1:]:
+    counter.update(c)
+
+#save counter in json
+with open('data/interim/umls_unigrams.json', 'w') as f:
+    json.dump(counter, f)
